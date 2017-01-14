679e4e6163033652aff5a51b167d5496eba435be
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2017-01-14 18:16:05 +0800

    Change indentation to four spaces

--- tutorials/2_minimum-spanning-tree/solution.cpp
+++ tutorials/2_minimum-spanning-tree/solution.cpp
@@ -9,29 +9,29 @@ pair<int,pair<int, int>> edges[MAXM + 5];
 int N, M;
 
 int root(int x) {
-  if (x == pt[x]) {
-    return x;
-  }
-  return pt[x] = root(pt[x]);
+    if (x == pt[x]) {
+        return x;
+    }
+    return pt[x] = root(pt[x]);
 }
 
 int main() {
-  scanf("%d %d", &N, &M);
-  for (int i = 0; i < N; ++i) {
-    pt[i] = i;
-  }
-  for (int i = 0; i < M; ++i) {
-    scanf("%d %d %d", &edges[i].second.first, &edges[i].second.second, &edges[i].first);
-  }
-  sort(edges, edges + M);
-  int mst = 0;
-  for (int i = 0; i < M; ++i) {
-    int u = edges[i].second.first;
-    int v = edges[i].second.second;
-    if (root(u) != root(v)) {
-      mst += edges[i].first;
-      pt[root(u)] = root(v);
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < N; ++i) {
+        pt[i] = i;
     }
-  }
-  printf("%d\n", mst);
+    for (int i = 0; i < M; ++i) {
+        scanf("%d %d %d", &edges[i].second.first, &edges[i].second.second, &edges[i].first);
+    }
+    sort(edges, edges + M);
+    int mst = 0;
+    for (int i = 0; i < M; ++i) {
+        int u = edges[i].second.first;
+        int v = edges[i].second.second;
+        if (root(u) != root(v)) {
+            mst += edges[i].first;
+            pt[root(u)] = root(v);
+        }
+    }
+    printf("%d\n", mst);
 }
